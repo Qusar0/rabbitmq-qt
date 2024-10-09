88ca4eea7a8ed74f464eae4bcb1a972e4b33fff6
Артем Ковалев <159487645+Qusar0@users.noreply.github.com>
2024-10-09 19:53:33 +0300

    Update __main__.py

--- client/rabbitmq_client/__main__.py
+++ client/rabbitmq_client/__main__.py
@@ -1,14 +1,62 @@
+from protobuf.message_pb2 import Response, Request
 import pika
+import uuid
+from PyQt5.QtCore import pyqtSignal, QObject, QThread
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+class RpcClient:
+    def __init__(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
 
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello, RabbitMQ')
+        self.channel = self.connection.channel()
 
-print(" [x] Sent 'Hello, RabbitMQ'")
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = result.method.queue
 
-connection.close()
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+
+        self.request_id = 1
+        self.response = None
+        self.corr_id = None
+
+    def on_response(self, ch, method, props, body):
+        self.response = Response()
+        self.response.ParseFromString(body)
+
+    def call(self, n, timeout):
+        request = Request(
+            return_address=self.callback_queue,
+            request_id=str(self.request_id),
+            proccess_time_in_seconds=timeout,
+            request=n
+        )
+
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='double',
+            routing_key='server_queue',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,
+            ),
+            body=request.SerializeToString())
+
+        self.request_id += 1
+
+        while self.response is None:
+            self.connection.process_data_events(time_limit=None)
+        return self.response
+
+
+fibonacci_rpc = RpcClient()
+
+i = int(input("Введите число: "))
+t = int(input('Введите задержку во времени: '))
+response = fibonacci_rpc.call(i, t)
+print(f"Ответ {response.response}")
